c3c97a0bab809e9151c6b2d21d4c60bd0d853ec2
Dzaky Rifai <76737872+dzaky-pr@users.noreply.github.com>
2021-07-27 07:13:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const typed = new Typed('.typing', {
+    strings: [' a Creative ', ' an Enthusiastic ', ' a Critical ', ' an Optimistic '],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
